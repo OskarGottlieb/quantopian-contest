76ae63192570a53af937f3ed2f6c195f3678ba7d
Oskar Gottlieb <gottlieboskar@gmail.com>
2018-11-03 14:20:17 -0400

    Improve UI, add new way of selectin quants

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from typing import Any, Dict, List, Optional
 
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
+from textwrap import dedent
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import plotly.graph_objs as go
-
+import settings.dev as settings
 
 
 class VisualApp:
+
 	def __init__(self) -> None:
 		self._dataframe = self._load_data()
 		self._quants: Dict[str, int] = self._get_quants_winnings()
@@ -41,7 +44,8 @@ class VisualApp:
 	def _get_dataframe_columns(self) -> List[Dict[str, str]]:
 		return [
 			{'label': self._beautify_column_name(column), 'value': column}
-			for column in self._dataframe.columns
+			for column in sorted(self._dataframe.columns, reverse = True)
+			if column not in settings.OMITTED_COLUMNS
 		]
 
 
@@ -63,50 +67,149 @@ class VisualApp:
 		traces = []
 		if column is not None:
 			traces = self._generate_traces(column, selected_quants = selected_quants)
+		reverse_y_axis = 'reversed' if column in settings.COLUMNS_WITH_REVERSED_Y_AXIS else True
 		return {
 			'data': traces,
 			'layout': {
-				'height': '800px',
-				'yaxis': {'autorange': 'reversed', 'range': [0, 10]}
+				'height': 700,
+				'title': self._beautify_column_name(column),
+				'yaxis': {'autorange': reverse_y_axis, 'side': 'right'},
 			}
 		}
 
 
 	def _init_app_content(self) -> dash.Dash:
-		self._app.layout = html.Div(
-				children=[
-		            html.H1(children='Hello Dash'),
 
-			dcc.Tabs(id="tabs", children=[
-				dcc.Tab(label='Historical plots', children=[
-					html.Div(id='', children=[
-				         dcc.Dropdown(
-					         id='quants',
-					         options=[{'label': quant, 'value': quant} for quant in self._quants.keys()],
-					         value=list(self._quants.keys())[:10],
-					         multi=True,
-				         ),
-				         dcc.Dropdown(
-					         id='column-select',
-					         options=self._get_dataframe_columns(),
-					         value='rank'
-				         ),
-			        ]),
-					dcc.Graph(id='main')
+		self._app.layout = html.Div(children = [
+			self._init_app_description(),
+			self._init_app_settings(),
+			dcc.Tabs(id="tabs", children = [
+				dcc.Tab(label='Historical plots', children = [
+					html.Div(children = [dcc.Graph(id='main')], className= 'container-fluid')
 				]),
-				dcc.Tab(label='Statistics', children=[
-					html.Div('lol', 'kek')
+				dcc.Tab(label='Statistics', children = [
+					html.Div('statistics', className='container-fluid')
 				])
-
-			]),
-		], className='container')
+			], className='container')
+		])
 
 		@self._app.callback(
 			Output('main', 'figure'),
-			[Input('column-select', 'value'), Input('quants', 'value')])
-		def generate_figure_of_selected_column(column_name: Optional[str], selected_quants: Optional[List[str]]):
+			[Input('dropdown_column', 'value'), Input('slider_rankings', 'value'), Input('dropdown_quants', 'value')],
+			[State('radio_select_quants', 'value')]
+		)
+		def generate_figure_of_selected_column(
+				column_name: Optional[str],
+				slider_rankings: Optional[List[int]],
+				dropdown_quants: Optional[List[str]],
+				radio_select_quants_value: str
+		):
+			'''
+
+			:param column_name: Column from the aggregated csv file
+			:param ranked_quants:
+			:return:
+			'''
+			if radio_select_quants_value == 'rank':
+				first_quant, last_quant = slider_rankings
+				selected_quants = list(self._quants.keys())[first_quant:last_quant]
+			else:
+				selected_quants = dropdown_quants
 			return self._generate_figure(column = column_name, selected_quants = selected_quants)
 
+		@self._app.callback(
+			Output('slider_rankings', 'disabled'),
+			[Input('radio_select_quants', 'value')])
+		def toggle_selection_of_quants_by_slider(radio_quant_select_value: str):
+			return radio_quant_select_value == 'name'
+
+		@self._app.callback(
+			Output('dropdown_quants', 'disabled'),
+			[Input('radio_select_quants', 'value')])
+		def toggle_selection_of_quants_by_nickname(radio_quant_select_value: str):
+			return radio_quant_select_value == 'rank'
+
+		@self._app.callback(
+			Output('dropdown_quants', 'value'),
+			[Input('slider_rankings', 'value')],
+			[State('radio_select_quants', 'value')])
+		def toggle_selection_of_quants_by_nickname(slider_rankings: Optional[List[int]], radio_select_quants_value: str):
+			if radio_select_quants_value == 'rank':
+				first_quant, last_quant = slider_rankings
+				return list(self._quants.keys())[first_quant:last_quant]
+
+
+	def _init_app_description(self) -> html.Div:
+		'''
+
+		'''
+		return html.Div(children = [
+			dcc.Markdown(dedent('''
+			     # Quantopian Contest Analytics
+			     The goal of this project is to look in more details at the Quantopian contestants.
+			'''))
+		], className='container')
+
+	def _init_app_settings(self) -> html.Div:
+		return html.Div(id='', children=[
+			html.Hr(),
+			dcc.Markdown(dedent('''
+				## Settings
+				By default 
+				Below you can select whether to look at quants, sorted by their cumulative PnL or you can handpick
+				the one specific which you want to look into.
+			''')),
+			html.Div(children = [
+				html.Div(children= [
+					dcc.Markdown(dedent('''
+						### Select Quants:
+					''')),
+					dcc.RadioItems(
+						id='radio_select_quants',
+						options=[
+							{'label': 'By cumulative PnL', 'value': 'rank'},
+							{'label': 'By nicknames', 'value': 'name'}
+						],
+						value='rank',
+					)
+				], className='col-md-3 col-sm-3, col-xs-12'),
+				html.Div(children=[
+					dcc.Markdown(dedent('''
+						#### Quants by Cumulative Winnings:
+					''')),
+					dcc.RangeSlider(
+						id='slider_rankings',
+						min=1,
+						max=len(self._quants),
+						marks={q: q for q in range(1, len(self._quants), 10)},
+						step=1,
+						value=[1, 10],
+					),
+				], className='col-md-9 col-sm-9, col-xs-12'),
+				html.Div(children=[
+					dcc.Markdown(dedent('''
+						### Value to plot:
+					''')),
+					dcc.Dropdown(
+						id='dropdown_column',
+						options = self._get_dataframe_columns(),
+						value='rank'
+					),
+				], className='col-md-3 col-sm-3, col-xs-12'),
+				html.Div(children=[
+					dcc.Markdown(dedent('''
+						#### Quants by Nickname:
+					''')),
+					dcc.Dropdown(
+						id='dropdown_quants',
+						options=[{'label': quant, 'value': quant} for quant in sorted(self._quants.keys())],
+						value=list(self._quants.keys())[:10],
+						multi=True,
+					),
+				], className='col-md-9 col-sm-9, col-xs-12'),
+			], className = 'row'),
+			html.Hr(),
+		], className = 'container')
 
 	def _init_app(self) -> None:
 		self._app = dash.Dash(
